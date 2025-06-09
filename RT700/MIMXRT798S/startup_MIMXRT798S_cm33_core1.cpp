b5c6e56a59f9ef60615a4ea02741fb16208683f2
Chay Guo <changyi.guo@nxp.com>
2025-06-09 15:59:25 +0800

    [MCUX-80525][RT700]Update C startup for IAR fixing code relocate issue with "initialize by copy"
    - This patch fixes some source are not relocated by "the initialize by copy" directive in linker.
    - Sections that are needed for initialization are not affected by the initialize by copy directive. This includes the __low_level_init function and anything it references.
      Anything reachable from the program entry label is considered needed for initialization unless reached via a section fragment with a label starting with __iar_init$$done.
      Define the symbol __iar_init$$done in C startup.
    
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- RT700/MIMXRT798S/startup_MIMXRT798S_cm33_core1.cpp
+++ RT700/MIMXRT798S/startup_MIMXRT798S_cm33_core1.cpp
@@ -355,6 +355,8 @@ extern void *__Vectors __attribute__((alias("g_pfnVectors")));
 __attribute__((used, section(".isr_vector"))) void (*const g_pfnVectors[])(void) = {
 #elif defined(__ICCARM__)
 extern void (*const __vector_table[])(void);
+/* The vector table is not needed for initialization. */
+extern void (*const __iar_init$$done[])(void) __attribute__((alias("__vector_table")));
 __attribute__((used, section(".intvec"))) void (*const __vector_table[])(void) = {
 #elif defined(__GNUC__)
 extern void (*const __isr_vector[])(void);
